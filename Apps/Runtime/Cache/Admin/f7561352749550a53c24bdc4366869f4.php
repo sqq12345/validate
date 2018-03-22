7fecf426913b978a1fa019f71a4024561c261ad7
fzf <ysfzf@hotmail.com>
2018-03-22 17:31:32 +0800

    git

--- Apps/Runtime/Cache/Admin/f7561352749550a53c24bdc4366869f4.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?>
-
-<?php if(is_array($data)): foreach($data as $k=>$v): ?><div title="<?php echo ($k); ?>" data-options="iconCls:'icon-<?php echo ($v['icon']); ?>'" style="padding:10px;">
-		<ul>
-			<?php if(is_array($v)): foreach($v as $key=>$v1): ?><li>
-					 <a href='<?php echo ($v1["url"]); ?>' data-icon='<?php echo ($v1["icon"]); ?>'><img src='<?php echo ($v1["img"]); ?>' style='width:16px;'/> <?php echo ($v1["title"]); ?> </a>
-				 </li><?php endforeach; endif; ?>
-		</ul>
-	</div><?php endforeach; endif; ?>
