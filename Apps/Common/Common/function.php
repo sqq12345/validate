c8760495fc3dcf7c6088973b005aedf5015e81d3
fzf <ysfzf@hotmail.com>
2018-03-27 08:57:17 +0800

    20180327

--- Apps/Common/Common/function.php
+++ Apps/Common/Common/function.php
@@ -24,18 +24,18 @@ function sendMail($to,$toname,$subject,$message){
     vendor('phpmailer.PHPMailerAutoload');
     $row=getConfig('mail_host,mail_port,mail_username,mail_pwd,mail_from,mail_fromname');
     $mail=new \PHPMailer();
-    $mail->isSMTP(); // 使用SMTP
-    $mail->SMTPDebug  = 0;  // 不开启调试
-    $mail->Host = $row['mail_host']; //SMTP地址
-    $mail->Port = $row['mail_port'];  //SMTP端口号 25
-    $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username = $row['mail_username']; // SMTP 用户名 '13886128089@163.com'
-    $mail->Password = $row['mail_pwd'] ; // SMTP 密码's123456'
+    $mail->isSMTP();  
+    $mail->SMTPDebug  = 0;   
+    $mail->Host = $row['mail_host'];  
+    $mail->Port = $row['mail_port'];   
+    $mail->SMTPAuth = true;  
+    $mail->Username = $row['mail_username'];  
+    $mail->Password = $row['mail_pwd'] ;  
     $mail->CharSet = 'utf-8';
-    $mail->From = $row['mail_username'];  //发件人邮箱'13886128089@163.com'
-    $mail->FromName =$row['mail_fromname']; //发件人姓名
-    $mail->addAddress($to,$toname); // 添加一个收件人
-    $mail->isHTML(true);// 使用HTML格式
+    $mail->From = $row['mail_username'];   
+    $mail->FromName =$row['mail_fromname'];  
+    $mail->addAddress($to,$toname);  
+    $mail->isHTML(true); 
     $mail->Subject = $subject;
     $message.='<hr /><p>'.$row['mail_fromname'].'</p><p>'.date('Y-m-d').'</p>';
     $mail->MsgHTML($message);
